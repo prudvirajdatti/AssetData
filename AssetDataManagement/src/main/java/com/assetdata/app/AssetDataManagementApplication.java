5d0e6e97d166ca07371a0476e7c3270f4133d568
Prudviraj Datti <datti.prudviraj@gmmil.com>
2020-06-25 17:19:50 +0530

    Added CORD handling

--- AssetDataManagement/src/main/java/com/assetdata/app/AssetDataManagementApplication.java
+++ AssetDataManagement/src/main/java/com/assetdata/app/AssetDataManagementApplication.java
@@ -2,12 +2,30 @@ package com.assetdata.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class AssetDataManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssetDataManagementApplication.class, args);
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();	
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter((CorsConfigurationSource) source);
 	}
 
 }
